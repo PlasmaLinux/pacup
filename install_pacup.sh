e00c5f8c932fb73d83ea08ff12079211b8f4b736
Fabric_Teacher <103819234+GoldWolv@users.noreply.github.com>
2022-09-11 11:22:52 +0900

    日本語化と初心者にわかりやすいように改良

--- install_pacup.sh
+++ install_pacup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ `whoami` != 'root' ]; then
-  echo "管理者権限で実行してください"
+  echo "sudoを先頭につけてください"
   exit
 fi
 
 sudo cp ./pacup.sh /usr/bin/pacup
 sudo chmod +x /usr/bin/pacup
-echo "Success!"
+echo "インストールは無事に完了しました。"
 
 exit
