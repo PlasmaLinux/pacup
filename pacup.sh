05ce592991f8770f2251d7622db332ff8f9e0d1c
Fabric_Teacher <103819234+GoldWolv@users.noreply.github.com>
2022-10-15 10:38:01 +0900

    バグの修正

--- pacup.sh
+++ pacup.sh
@@ -6,11 +6,27 @@ Usage: $(basename "$0") [OPTION]...
 EOM
 
     exit 2
+}
+
+function update_y() {
+    sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
+    exit 0
+}
 
 if [ `whoami` != 'root' ]; then
-    echo "管理者権限で実行してください"
+    echo "sudoを先頭につけてください"
     exit 0
 fi
 
+while getopts ":h:y" optKey; do
+    case "$optKey" in
+    	y)
+    	  update_y
+          ;;    	
+    	'-h'|'--help'|* )
+    	  usage
+    	  ;;
+    esac
+done
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 exit 0
