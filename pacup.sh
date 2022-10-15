04c46a4dafd18105adbd7fc0210a86d4f0059ad9
Jotch-96 <107109450+Plasma-Linux@users.noreply.github.com>
2022-10-15 10:22:46 +0900

    Update pacup.sh

--- pacup.sh
+++ pacup.sh
@@ -7,11 +7,6 @@ EOM
 
     exit 2
 
-function update_full() {
-    apt update && apt full-upgrade -y && sudo apt autoremove
-    exit 0
-}
-
 if [ `whoami` != 'root' ]; then
     echo "sudoを先頭につけてください"
     exit 0
@@ -19,4 +14,3 @@ fi
 
 apt update && apt full-upgrade -y && sudo apt autoremove -y
 exit 0
-
