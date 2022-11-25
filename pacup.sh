5865179d4bd37bc351eb1d72506dccffaed78836
Fabric_Teacher <103819234+GoldWolv@users.noreply.github.com>
2022-11-26 08:41:00 +0900

    管理者権限を不要にするよう修正

--- pacup.sh
+++ pacup.sh
@@ -6,27 +6,8 @@ Usage: $(basename "$0") [OPTION]...
 EOM
 
     exit 2
+    
 }
 
-function update_y() {
-    sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
-    exit 0
-}
-
-if [ `whoami` != 'root' ]; then
-    echo "sudoを先頭につけてください"
-    exit 0
-fi
-
-while getopts ":h:y" optKey; do
-    case "$optKey" in
-    	y)
-    	  update_y
-          ;;    	
-    	'-h'|'--help'|* )
-    	  usage
-    	  ;;
-    esac
-done
 sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y
 exit 0
